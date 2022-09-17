96683d1736b0a7ec48e07555d0b07e4139063053
Drei4 <antipin-lukinf@mail.ru>
2022-09-17 17:53:39 +0300

    add method PrintArray

--- Program.cs
+++ Program.cs
@@ -32,8 +32,18 @@ void СompletionArray2 (string[] array1, string[] array2)
     }
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
 
     
 СompletionArray2 (array1, array2);
-//PrintArray (array2);
+PrintArray (array2);
 
